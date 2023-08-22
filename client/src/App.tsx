66aa35f948f396d68c607a5164b23e2c36e68f22
Kevin <kevinyoon01@gmail.com>
2023-08-22 16:54:01 -0700

    Removed basic css files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
-import './App.css';
 
 function App() {
   return (
