19f1be1a094d65f97370b41dac8e767999d955c5
larkinaj <larkin.aj@gmail.com>
2023-08-22 22:15:36 -0400

    added tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
     </>
---
+++ client/src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <>
+      <main>
+        <div>
+          <img src="https://avatars.githubusercontent.com/u/31088037?v=4" alt='profile'></img>
+          <h3>Andrew Larkin</h3>
+        </div>
+        <div>
+          <div>
+            <p>25</p>
+            <p>Recs</p>
+          </div>
+          <div>
+            <p>10</p>
+            <p>Followers</p>
+          </div>
+          <div>
+            <p>35</p>
+            <p>Following</p>
+          </div>
+        </div>
+      </main>
+
+    </>
+  )
+}
+
+export default Profile;
