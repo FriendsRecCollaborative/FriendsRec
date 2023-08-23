ce74357bcc85b902497f092211a13277ad61c390
William Nguyen <105384182+wnguye03@users.noreply.github.com>
2023-08-22 21:36:20 -0400

    finished boiler plate for expres backend

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import express, { Express, NextFunction, Request, Response, ErrorRequestHandler } from 'express';
+
+const PORT: number = 8080;
+
+const app: Express = express();
+
+app.use(express.json());
+
+
+// express general error handler
+app.use((err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+    const defaultError: DefaultError = {
+        log: 'error at unknown middleware',
+        code: 500,
+        message: 'check error'
+    }
+
+    const newErr = Object.assign({}, defaultError, err);
+    console.log(err);
+    return res.status(newErr.code).json(newErr.message);
+});
+
+app.listen(PORT, (): void => {
+    console.log('Server running on on port:' + PORT)
+});
