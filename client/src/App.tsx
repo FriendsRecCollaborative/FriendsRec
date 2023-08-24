7330c0d89049a84b0dcd63274debb4c600e4b3c7
Kevin <kevinyoon01@gmail.com>
2023-08-23 19:33:15 -0700

    Completed side navbar with react router dom set up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import Search from './pages/Search';
+import Friends from './pages/Friends';
+import Profile from './pages/Profile';
 import '../src/index.css';
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/searchrecs" element={<Search />} />
+          <Route path="/friends" element={<Friends />} />
+          <Route path="/myprofile" element={<Profile />} />
         </Routes>
       </Router>
     </>
