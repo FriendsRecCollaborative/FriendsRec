d4270721cb6ed24727acb15c059570320bba2a21
Kevin <kevinyoon01@gmail.com>
2023-08-29 19:35:03 -0700

    Updated min height to show all

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -55,10 +55,10 @@ function Home() {
 
   return (
     <>
-      <div className="flex h-screen bg-gray-50">
+      <div className="flex bg-gray-50">
         <Sidebar />
         <div className="flex-1 ml-80">
-          <div className="mr-64 p-20 pt-24 border-r-[1.5px] h-full right-0">
+          <div className="mr-64 p-20 pt-24 border-r-[1.5px] h-full right-0 bg-gray-50">
             <time className="text-lg font-semibold text-gray-900 dark:text-white">This Week</time>
             <div className="mt-3 p-5 mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
               {thisWeek.map((item) => (
